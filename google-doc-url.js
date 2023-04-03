270cae19639b432f7fd9dbb296a7756c205bd7dd
rachelkim12 <128184759+rachelkim12@users.noreply.github.com>
2023-04-03 19:47:23 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSy-7ir1XN4HdDr7U8tPC8QRrBWYq7mibxaQiZjMCJjGoPrVwLCXWJsA6RLqTpYefvlltd4sajw_k0H/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
